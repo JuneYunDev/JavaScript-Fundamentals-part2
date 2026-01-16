7ef5ca8ec4e6ecee51645a7d739c6d3685dc7647
June Yun <97yunjy@gmail.com>
2026-01-15 23:17:53 -0700

    Bonus

--- Challenge#2.js
+++ Challenge#2.js
@@ -26,3 +26,6 @@ const bills = [125, 55, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length-1])];
 console.log(tips);
 
+
+const total = [bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]]
+console.log(total);
