3dfe6963a7e6d7b79335e58f71cb69a92d4e5493
June Yun <97yunjy@gmail.com>
2026-01-18 00:21:55 -0700

    Bonus Answer

--- Challenge#4.js
+++ Challenge#4.js
@@ -46,7 +46,10 @@ calcTips();
 //Bonus
 function calcAverage(arr){
     let sum = 0
-    for(let i = 0; i < arr.length-1; i++){
+    for(let i = 0; i < arr.length; i++){
         sum = sum + arr[i];
     }
-}
+    return sum / arr.length
+  }
+
+console.log(calcAverage(totals));
